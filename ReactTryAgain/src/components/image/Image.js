ed19e756741c3b1cc1fcca81240f490a7f4ce808
Refik Gulak <74262442+Refik48@users.noreply.github.com>
2022-12-23 16:13:35 +0000

    day23

--- ReactTryAgain/src/components/image/Image.js
+++ ReactTryAgain/src/components/image/Image.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from "react";
+import image2 from "../../assets/img/image2.jpg";
 
 const Image = () => {
+  console.log(require("../../assets/img/image3.jpg"));
   return (
     <div>
       <div>
-       <img src="assets/img/image1.jpg"/>
+      <h3>Statik Yontemi</h3>
+        <img src="assets/img/image1.jpg" />
+      </div>
+      <div>
+        <h3>Import Yontemi</h3>
+        <img src={image2}/>
+      </div>
+      <div>
+        <h3>Require Yontemi</h3>
+        <img src={require("../../assets/img/image3.jpg").default}/>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Image
+export default Image;
