f943ffbdd8e192cfdcd516e74293402fe117dc2e
Refik Gulak <74262442+Refik48@users.noreply.github.com>
2022-12-30 15:54:14 +0000

    day25

--- ReactTryAgain/src/components/state/State.js
+++ ReactTryAgain/src/components/state/State.js
@@ -3,11 +3,14 @@ import { Button } from "react-bootstrap";
 // 1. onceliklee useState import edilmeli
 const State = () => {
   const [sayac, setSayac] = useState();
+  const artir=() =>{
+     setSayac(sayac + 1);
+  }
 
   return (
     <div>
       <h1>Sayac degeri: {sayac}</h1>
-      <Button variant="info" onClick={() => setSayac(sayac + 1)}>
+      <Button variant="info" onClick={() => setSayac(artir)}>
         Artir
       </Button>
     </div>
