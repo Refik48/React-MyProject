06200925a433b216dfbb28f442d4db37e126d62f
Refik Gulak <74262442+Refik48@users.noreply.github.com>
2023-01-02 22:02:10 +0000

    day26

--- ReactTryAgain/src/components/clock3/Clock3.js
+++ ReactTryAgain/src/components/clock3/Clock3.js
@@ -1,12 +1,16 @@
 import "./clock3.css";
 import moment from "moment";
+import React, { useState, useEffect } from "react";
 
 const Clock3 = (props) => {
-  const dateTime = moment();
+  const [dateTime, setDateTime] = useState(moment());
+  // const dateTime = moment();
   const timeStr = dateTime.format("HH:mm");
   const dateStr = dateTime.format("LL");
   const dayStr = dateTime.format("dddd");
   const hour = dateTime.format("HH");
+  const minute = dateTime.format("mm");
+const second = dateTime.format("ss");
 
   let message = "";
   if (hour > 6 && hour < 11) message = "Morning";
@@ -14,12 +18,31 @@ const Clock3 = (props) => {
   else if (hour >= 17 && hour < 21) message = "Evening";
   else message = "Night";
 
+  useEffect(() => {
+    const sayac = setInterval(() => {
+      console.log("Deneme");
+      setDateTime(moment());
+    }, 1000);
+
+    return () => {
+      clearInterval(sayac);
+    };
+  }, []);
+
+  // setTimeout( ()=>{console.log("Merhaba");},1000)
+  // setInterval(() => {
+  //   setDateTime(moment());
+  // }, 1000);
+
   return (
     <div
       className="clock-container"
       style={{ color: props.yaziRengi, backgroundColor: props.zeminRengi }}
     >
-      <div className="clock">{timeStr}</div>
+      <div className="clock">
+      {hour}
+       {second % 2 == 0 ? ":" :  " "}
+      {minute}</div>
       <div>
         <div className="date">{dateStr}</div>
         <div className="day">
